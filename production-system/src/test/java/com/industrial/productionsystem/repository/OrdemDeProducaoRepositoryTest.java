bfcc71d113c8f02c222425a78c8216dcbc81e0c1
JVC2511 <jvacd2003@gmail.com>
2026-04-10 16:22:48 -0300

    Adicionando 4 testes unítarios

--- production-system/src/test/java/com/industrial/productionsystem/repository/OrdemDeProducaoRepositoryTest.java
+++ production-system/src/test/java/com/industrial/productionsystem/repository/OrdemDeProducaoRepositoryTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.industrial.productionsystem.repository.MaquinaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
